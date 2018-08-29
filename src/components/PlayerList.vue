42e213434fae45c0c0386d337e780d96ea4582ac
jocelynhsu <rulinshyu@gmail.com>
2018-08-30 02:26:06 +0800

    feat(web): adjust grid layout

--- src/components/PlayerList.vue
+++ src/components/PlayerList.vue
@@ -24,6 +24,20 @@ export default {
   padding: 10px;
   grid-gap: 10px;
   grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: 1fr;
+
+  &::before {
+    content: '';
+    width: 0;
+    padding-bottom: 100%;
+    grid-row: 1 / 1;
+    grid-column: 1 / 1;
+  }
+
+  > *:first-child {
+    grid-row: 1 / 1;
+    grid-column: 1 / 1;
+  }
 
   @media (min-width: 500px) { grid-template-columns: repeat(4, 1fr); }
   @media (min-width: 600px) { grid-template-columns: repeat(5, 1fr); }
